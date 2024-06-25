b08a231fe93f4c14a81a7e782a4eea32817a6374
billy54197 <billy54197@126.com>
2024-06-25 22:36:37 +0800

    submit code

--- assets/prd-province-api-ac081789.js
+++
@@ -1,1 +0,0 @@
-import{v as n,__tla as o}from"./index-d2e32ea4.js";let e,c=Promise.all([(()=>{try{return o}catch{}})()]).then(async()=>{let a;a="/portalPrdProvince",e=async()=>new Promise((r,l)=>{n(`${a}/getProvinceData`).then(t=>{r(t.data)}).catch(t=>{l(t)})})});export{c as __tla,e as g};
